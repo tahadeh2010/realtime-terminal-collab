eb81c1fe1abf843829506a3f7e7ba8ef8e5d70ed
Taha <tahadehnavi8989@gmail.com>
2026-06-24 21:26:28 +0330

    move ConnectionManager to application layer

---
+++ internal/application/connection_manager.go
@@ -0,0 +1,48 @@
+package application
+
+import (
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type Connection struct {
+	ID   string
+	Conn *websocket.Conn
+}
+
+type ConnectionManager struct {
+	mu          sync.RWMutex
+	connections map[string]*Connection
+}
+
+func NewConnectionManager() *ConnectionManager {
+	return &ConnectionManager{
+		connections: make(map[string]*Connection),
+	}
+}
+
+func (cm *ConnectionManager) Register(id string, conn *websocket.Conn) {
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
+
+	cm.connections[id] = &Connection{
+		ID:   id,
+		Conn: conn,
+	}
+}
+
+func (cm *ConnectionManager) Unregister(id string) {
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
+
+	delete(cm.connections, id)
+}
+
+func (cm *ConnectionManager) GetConnection(id string) (*Connection, bool) {
+	cm.mu.RLock()
+	defer cm.mu.RUnlock()
+
+	conn, ok := cm.connections[id]
+	return conn, ok
+}
